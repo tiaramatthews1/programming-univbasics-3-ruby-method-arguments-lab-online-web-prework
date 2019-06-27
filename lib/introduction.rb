be013f1a7fa0ba4f99ea25009c69d3f8007a6dd8
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-27 20:17:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,4 +8,8 @@ introduction ("Josh")
 introduction ("Dan")
 # > Hi, my name is Dan.
 
-def
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language ("Dan", language)
