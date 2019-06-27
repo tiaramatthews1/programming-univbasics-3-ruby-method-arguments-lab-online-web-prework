4c9d44f5c4c7c661e24cd88531b39efe0f39b82b
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-27 21:10:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -23,7 +23,7 @@ def introduction_with_language_optional(name, language)
 end
 
 introduction_with_language("Edwin")
-# > Hi, my name is Edwin and I am learning to program in Ruby.
+# > Hi, my name is Edwin and I am learning to program.
 
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
