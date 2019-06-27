8ad2806bd0f6b1c535eab7ba460a4cb9f5fa5488
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-27 19:48:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
-def introduction(name)
+def introduction("Josh")
   "Hi, my name is #{name} and I and learning to program in ."
 end
 
-def introduction(name)
+def introduction(Dan")
   "Hi, my name is #{name}."
 end
